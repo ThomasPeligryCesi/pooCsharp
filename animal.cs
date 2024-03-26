1ffd108c17d0c39f770ad566057ee7fecd0cf6f7
Thomas Peligry <thomas.peligry@viacesi.fr>
2024-03-26 09:23:40 +0100

    classe animal

---
+++ animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Animal
+{
+    public Animal()
+    {
+        this.Name = "Random Animal";
+    }
+    public Animal(string name)
+    {
+        this.Name = name;
+    }
+    private string Name;
+    public void move()
+    {
+        console.WriteLine(Name + "se dandine et se déplace!");
+    }
+    public void Sleep()
+    {
+        console.WriteLine(Name + "s'est endormi et ronfle fort!");
+    }
+}
+
